0b1f4c2cd8c38f70841a5c2421260bd615201b59
hanui_asi <l.lise413@gmail.com>
2022-07-26 17:45:47 +0900

    [0726] fetch API 1 (실습 4-3 오류)

--- Programers/0726/src/Cart.js
+++ Programers/0726/src/Cart.js
@@ -20,24 +20,46 @@ export default function Cart({ $target, initialState, onRemove }) {
 
    const calculateTotalPrice = () => {
       const { basePrice, selectedOptions } = this.state
-      
+
       return selectedOptions.reduce((acc, option) => 
          acc + ((basePrice + option.optionPrice) * option.ea), 0
       )
    }
 
+   const renderOption = (option, index) => {
+      const { productName, basePrice } = this.state
+      return  `
+         <li data-index="${index}" class="cartItem">
+            ${productName} - ${option.optionName} | ${basePrice + option.optionPrice}, ${option.ea}개 
+               <button class="remove"> X </button>
+         </li>
+      `
+   }
+
    this.render = () => {
-      const { productName, basePrice, selectedOptions } = this.state
+      const { selectedOptions } = this.state
       $cart.innerHTML = `
          <ul>
-            ${Array.isArray(selectedOptions) && selectedOptions.map(option => `
-               <li>${productName} - ${option.optionName} | ${basePrice + option.optionPrice}원, ${option.ea}개</li>
-            `).join('')}
+            ${Array.isArray(selectedOptions) && selectedOptions.map((option, index) => 
+               renderOption(option, index)
+            ).join('')}
          </ul>
          <div>
             ${calculateTotalPrice()}원
          </div>
       `
+
+      $cart.querySelectorAll('.remove').forEach($button => {
+         $button.addEventListener('click', (e) => {
+            const $li = e.target.closest('.carItem') 
+            
+            if ($li) {
+               const { index } = $li.dataset
+
+               alert(index)
+            }
+         })
+      })
    }
    this.render()
 }
