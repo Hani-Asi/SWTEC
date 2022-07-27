91513912b2d96e1c4ad601e901d82307ac33b318
hanui_asi <l.lise413@gmail.com>
2022-07-27 09:39:40 +0900

    [0708] 해시 테이블

---
+++ Programers/0708/해시 테이블.js
@@ -0,0 +1,14 @@
+const table = new Map();
+table.set("key", 100);
+table.set("key2", "Hello");
+console.log(table["key"]);
+console.log(table.get("key"));
+const object = { a: 1 };
+table.set(object, "A1");
+console.log(table.get(object));
+table.delete(object);
+console.log(table.get(object));
+console.log(table.keys());
+console.log(table.values());
+table.clear();
+console.log(table.values());
