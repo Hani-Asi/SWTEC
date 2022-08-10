5139583b3dafeacd6f25c1b8a45cf9a457a9255e
hanui_asi <l.lise413@gmail.com>
2022-08-10 10:00:52 +0900

    [0808] react useEffect

--- Programers/0808/asis2-app/src/App.js
+++ Programers/0808/asis2-app/src/App.js
@@ -1,25 +1,33 @@
-/*  요구사항
-    1. Counter 컴포넌트 구현
-    2. 모든 Counter 컴포넌트의 합 구하기
+/*  
+    useEffect는 무언가 변화가 있을 때
+    감지하여 반응하는 Hook
 */
 
-import { useState } from 'react'
-import Counter from './components/Counter'
+import { useEffect, useState } from 'react'
 
 export default function App() {
-  const [totalCount, setTotalCount] = useState(0)
+  const [count, setCount] = useState(0)
 
+  useEffect(() => {
+    console.log(`Clicked ${count} times`)
+  }, [count]) // count의 변화를 감지한다
+
+  useEffect(() => {
+    console.log("Component Loaded")
+    const handleSroll = () => {
+      console.log(window.scrollY)
+    }
+
+    document.addEventListener("scroll", handleSroll) // 전역적인 이벤트를 사용할 때 쓸 수 있다
+    return () => document.removeEventListener('scroll', handleSroll) // return으로 변환한 함수는 컴포넌트가 제거될 때 실행된다
+  }, []) // 컴포넌트가 처음 로드될 때 실행된다
+  
   return (
     <div>
-      TotalCount: {totalCount}
-      <Counter 
-        onIncrease={(count) => {setTotalCount(totalCount + 1)}}
-        onDecrease={(count) => {setTotalCount(totalCount - 1)}}
-      />
-      <Counter 
-        onIncrease={(count) => {setTotalCount(totalCount + 1)}}
-        onDecrease={(count) => {setTotalCount(totalCount - 1)}}
-      />
+      <div>You clicked {count} times</div>
+      <button onClick={() => setCount(count + 1)}>+</button>
+
+      <div style={{ heigh: 10000 }}></div>
     </div>
   )
 }
