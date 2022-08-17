024dd50fc2bd33b372d308948048faea11a4b7b7
hanui_asi <l.lise413@gmail.com>
2022-08-17 11:08:42 +0900

    [0809] react React.memo

--- Programers/0808/asis2-app/src/App.js
+++ Programers/0808/asis2-app/src/App.js
@@ -1,19 +1,19 @@
-/*  useMemo
+/*  React.memo
     1. 함수 컴포넌트는 자신의 상태가 변경될 때 리렌더링된다.
     2. 부모 컴포넌트로 부터 받는 prop이 변경될 때 리렌더링된다.
     3. 부모 컴포넌트의 상태가 변경될 때 리렌더링된다.
 */
-
 import { useState } from "react"
-import ShowSum from "./components/ShowSum"
+import Box from "./components/Box"
 
 export default function App() {
-  const [label, setLabel] = useState("Result")
+  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <button onClick={() => setLabel(label + ":")}>Change Label</button>
-      <ShowSum label={label} n={1000000000} />
+      {count}
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <Box />
     </div>
   )
 }
