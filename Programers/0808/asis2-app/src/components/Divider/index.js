f856893ef3a4d8bbc93af5f9ec5b4bd1d905b848
Hanui-Asi <rutilio413@naver.com>
2022-08-30 17:34:26 +0900

    [0815] react storybook Divider

--- Programers/0808/asis2-app/src/components/Divider/index.js
+++ Programers/0808/asis2-app/src/components/Divider/index.js
@@ -12,13 +12,26 @@ const Line = styled.hr`
     height: 13px;
     vertical-align: middle;
   }
+
+  &.horizontal {
+    display: block;
+    width: 100%;
+    height: 1px;
+  }
 `;
 
 const Divider = ({ type = "horizontal", size = 8, ...props }) => {
   const dividerStyle = {
-    margin: (type = "vertical" ? `0 ${size}px` : `${size}px 0`),
+    margin: type === "vertical" ? `0 ${size}px` : `${size}px 0`,
   };
-  return <hr {...props} style={{ ...dividerStyle, ...props.style }} />;
+
+  return (
+    <Line
+      {...props}
+      className={type}
+      style={{ ...dividerStyle, ...props.style }}
+    />
+  );
 };
 
 export default Divider;
