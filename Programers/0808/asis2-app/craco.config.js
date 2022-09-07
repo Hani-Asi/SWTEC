74006b155b4eea1bfd230e6804c88edc44f9027a
Hanui-Asi <l.lise413@gmail.com>
2022-09-08 00:56:16 +0900

    [0818] react-router

--- Programers/0808/asis2-app/craco.config.js
+++ Programers/0808/asis2-app/craco.config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   webpack: {
     alias: {
-      "@componenets": path.resolve(__dirname, "src/components"),
+      "@components": path.resolve(__dirname, "src/components"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
       "@contexts": path.resolve(__dirname, "src/contexts"),
       "@pages": path.resolve(__dirname, "src/pages"),
--- Programers/0808/asis2-app/src/App.js
+++ Programers/0808/asis2-app/src/App.js
@@ -1,47 +1,17 @@
-import axios from "axios";
-import { useAsync } from "./hooks";
-import { Header, Spinner } from "./components";
-import PostList from "./components/domain/PostList";
-import PostProvider from "./contexts/PostProvider";
-import { useCallback } from "react";
-import PostAddForm from "./components/domain/PostAddForm";
-
-// 컴포넌트는 최대한 순수할 수록 좋다.
-// 1. 사이드 이펙트를 걱정하지 ㅇ낳아도 된다.
-// 2. 확장에 유연하다.
-// 3. 테스트가 쉽다.
+import { Route, Routes } from "react-router-dom";
+import { PostsPage, PostPage, NotFoundPage } from "@pages";
+import DefaultTemplate from "@components/template/DefaultTemplate";
 
 const App = () => {
-  const initialPosts = useAsync(async () => {
-    return await axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.data);
-  }, []);
-
-  const handleAddPost = useCallback(async (post) => {
-    return await axios
-      .post(`https://jsonplaceholder.typicode.com/posts`, post)
-      .then((response) => response.data);
-  }, []);
-
-  const handleDeletePost = useCallback(async (id) => {
-    return await axios
-      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then(() => ({ id }));
-  }, []);
-
   return (
-    <PostProvider
-      initialPosts={initialPosts.value}
-      handleDeletePost={handleDeletePost}
-      handleAddPost={handleAddPost}
-    >
-      <div>
-        <Header>Posts</Header>
-        <PostAddForm />
-        {initialPosts.isLoading ? <Spinner /> : <PostList />}
-      </div>
-    </PostProvider>
+    <DefaultTemplate>
+      <Routes>
+        <Route path="/" exact element={<h1>Home</h1>} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:postId" element={<PostPage />} />
+        <Route path="*" element={<NotFoundPage />} /> // 제일 마지막에 있어야함
+      </Routes>
+    </DefaultTemplate>
   );
 };
 
