b4cf253bf8853acf9831bd415b067430d36dadde
Hanui-Asi <rutilio413@naver.com>
2022-09-07 11:26:57 +0900

    [0818] react axios error

--- Programers/0808/asis2-app/src/App.js
+++ Programers/0808/asis2-app/src/App.js
@@ -1,3 +1,29 @@
-export default function App() {
-  return <div>learn component</div>;
-}
+import axios from "axios";
+import useAsync from "./hooks/useAsync";
+import Header from "./components/Header";
+import Spinner from "./components/Spinner";
+
+const App = () => {
+  const initialPosts = useAsync(async () => {
+    return await axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.date);
+  }, []);
+
+  return (
+    <div>
+      <Header>Posts</Header>
+      <ul>
+        {initialPosts.isLoading ? (
+          <Spinner />
+        ) : (
+          (initialPosts.value || []).map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
