df3651e4f49ad547489c7fd1721309215f850d9a
Hanui-Asi <rutilio413@naver.com>
2022-09-14 14:13:23 +0900

    [0823] NEXT.js / storybook / init

--- Programers/0823/nextjs/pages/about/index.tsx
+++ Programers/0823/nextjs/pages/about/index.tsx
@@ -1,5 +1,20 @@
+import styled from "@emotion/styled";
+import Link from "next/link";
+
 const About = () => {
-  return <div>Hello there</div>;
+  return (
+    <div>
+      Hello there
+      <Box />
+      <Link href="/">Go to home</Link>
+    </div>
+  );
 };
 
 export default About;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: red;
+`;
