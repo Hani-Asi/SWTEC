6d8e8ceb64c5b91127299ffbf7cf9ee9891506ea
Hanui-Asi <rutilio413@naver.com>
2022-09-14 15:12:51 +0900

    [0823] Next.js / ServerSide data Training error

--- Programers/0823/nextjs/pages/posts/[id].tsx
+++ Programers/0823/nextjs/pages/posts/[id].tsx
@@ -1,14 +1,40 @@
-import { useRouter } from "next/router";
+import axios from "axios";
+import { NextPageContext } from "next";
+import { Post } from "../../interfaces";
 
-const Post = () => {
-  const router = useRouter();
+export const getServerSideProps = async (context: NextPageContext) => {
+  const postId = context.query.id;
 
+  try {
+    const { data: post } = await axios(
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+
+    return {
+      props: { post },
+    };
+  } catch (e) {
+    if (axios.isAxiosError(e) && e.response?.status === 404) {
+      return {
+        notFound: true,
+      };
+    }
+
+    return { props: {} };
+  }
+};
+
+interface Props {
+  post: Post;
+}
+
+const PostPage = ({ post }: Props) => {
   return (
     <div>
-      <button onClick={() => router.push("/about")}>Go to about</button>
-      {router.query.id}
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
     </div>
   );
 };
 
-export default Post;
+export default PostPage;
