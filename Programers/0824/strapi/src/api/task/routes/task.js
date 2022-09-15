036469e94b4c049b368413498cd590ae8a7e83a9
Hanui-Asi <rutilio413@naver.com>
2022-09-15 14:35:46 +0900

    [0825] graphql-todo / init

--- Programers/0824/strapi/src/api/task/routes/task.js
+++ Programers/0824/strapi/src/api/task/routes/task.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * post router
+ * task router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::post.post');
+module.exports = createCoreRouter('api::task.task');
--- Programers/0824/strapi/src/api/task/services/task.js
+++ Programers/0824/strapi/src/api/task/services/task.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * post service
+ * task service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::post.post');
+module.exports = createCoreService('api::task.task');
