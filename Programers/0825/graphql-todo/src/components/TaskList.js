0d70f348da025a0558119eba238f4ddc39b6da31
Hanui-Asi <rutilio413@naver.com>
2022-09-15 16:04:59 +0900

    [0825] graphql-todo / Training2

---
+++ Programers/0825/graphql-todo/src/components/TaskList.js
@@ -0,0 +1,52 @@
+import { gql, useQuery } from "@apollo/client";
+import styled from "@emotion/styled";
+import { Task } from "./Task";
+
+const UnorderedList = styled.ul`
+  width: 400px;
+  margin: 0;
+  margin-top: 16px;
+  padding: 0;
+
+  & > li {
+    &:not(:first-of-type) {
+      margin-top: 8px;
+    }
+  }
+`;
+
+const GET_TASK = gql`
+  query GetTasks {
+    tasks {
+      data {
+        id
+        attributes {
+          content
+          complete
+        }
+      }
+    }
+  }
+`;
+
+export const TaskList = () => {
+  const { data, loading, error } = useQuery(GET_TASK);
+
+  if (loading) return "Loading...";
+  if (error) return "Error";
+
+  return (
+    <UnorderedList>
+      {data.tasks.data.map((task) => {
+        return (
+          <Task
+            key={task.id}
+            id={task.id}
+            content={task.attributes.content}
+            complete={task.attributes.complete}
+          />
+        );
+      })}
+    </UnorderedList>
+  );
+};
