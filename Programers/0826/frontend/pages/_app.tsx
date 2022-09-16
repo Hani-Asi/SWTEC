1ad706bd487c8a4a2ca567bf5c1e17e97b29744a
Hanui-Asi <rutilio413@naver.com>
2022-09-16 15:44:26 +0900

    [0826] felog Blog / Next.js / frontend 1

--- Programers/0826/frontend/pages/_app.tsx
+++ Programers/0826/frontend/pages/_app.tsx
@@ -1,7 +1,29 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { CoProvider, AppShell } from "@co-design/core";
 import type { AppProps } from "next/app";
+import { Header } from "../component";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const header = (
+    <AppShell.Header height={70}>
+      <Header />
+    </AppShell.Header>
+  );
+
+  return (
+    <ApolloProvider client={client}>
+      <CoProvider withNormalizeCSS withGlobalStyles>
+        <AppShell fixed header={header}>
+          <Component {...pageProps} />
+        </AppShell>
+      </CoProvider>
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
--- Programers/0826/frontend/pages/index.tsx
+++ Programers/0826/frontend/pages/index.tsx
@@ -1,7 +1,68 @@
-import type { NextPage } from "next";
+import { gql, useQuery } from "@apollo/client";
+import {
+  Card,
+  Heading,
+  Text,
+  Divider,
+  EquallyGrid,
+  Spinner,
+  Center,
+  Container,
+  Anchor,
+} from "@co-design/core";
+import NextLink from "next/link";
+
+const GET_POSTS = gql`
+  query GetPost {
+    posts {
+      data {
+        id
+        attributes {
+          title
+          body
+          createdAt
+          user {
+            data {
+              attributes {
+                username
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Home = () => {
-  return <div></div>;
+  const { data, loading, error } = useQuery(GET_POSTS);
+
+  return (
+    <Container padding={16} co={{ marginTop: 16 }}>
+      {loading ? (
+        <Center>
+          <Spinner />
+        </Center>
+      ) : (
+        <EquallyGrid cols={4}>
+          {data.posts.data.map((post: any) => (
+            <Card key={post.id}>
+              <NextLink href="/posts/[id]" as={`/posts/${post.id}`} passHref>
+                <Anchor co={{ textDecoration: "none", color: "black" }}>
+                  <Heading level={4}>{post.attributes.title}</Heading>
+                </Anchor>
+              </NextLink>
+              <Text lineClamp={3}>{post.attributes.body}</Text>
+              <Divider />
+              <Text block align="right">
+                {post.attributes.user.data.attributes.username}
+              </Text>
+            </Card>
+          ))}
+        </EquallyGrid>
+      )}
+    </Container>
+  );
 };
 
 export default Home;
