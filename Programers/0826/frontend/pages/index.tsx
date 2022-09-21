5fdaa79aaf54276634cb6770a095d2d0e20bb67e
Hanui-Asi <rutilio413@naver.com>
2022-09-21 16:49:40 +0900

    [0826] felog Blog / Next.js / frontend / SignUp Fix and CreatePost

--- Programers/0826/frontend/pages/index.tsx
+++ Programers/0826/frontend/pages/index.tsx
@@ -9,12 +9,16 @@ import {
   Center,
   Container,
   Anchor,
+  Stack,
+  View,
+  Button,
 } from "@co-design/core";
 import NextLink from "next/link";
+import { NextLinkComposed } from "../component";
 
 const GET_POSTS = gql`
   query GetPost {
-    posts {
+    posts(sort: ["createdAt:desc"]) {
       data {
         id
         attributes {
@@ -34,7 +38,11 @@ const GET_POSTS = gql`
   }
 `;
 
-const Home = () => {
+interface Props {
+  token?: string;
+}
+
+const Home = ({ token }: Props) => {
   const { data, loading, error } = useQuery(GET_POSTS);
 
   return (
@@ -44,22 +52,31 @@ const Home = () => {
           <Spinner />
         </Center>
       ) : (
-        <EquallyGrid cols={4}>
-          {data.posts.data.map((post: any) => (
-            <Card key={post.id}>
-              <NextLink href="/posts/[id]" as={`/posts/${post.id}`} passHref>
-                <Anchor co={{ textDecoration: "none", color: "black" }}>
-                  <Heading level={4}>{post.attributes.title}</Heading>
-                </Anchor>
-              </NextLink>
-              <Text lineClamp={3}>{post.attributes.body}</Text>
-              <Divider />
-              <Text block align="right">
-                {post.attributes.user.data.attributes.username}
-              </Text>
-            </Card>
-          ))}
-        </EquallyGrid>
+        <Stack>
+          {token && (
+            <View co={{ textAlign: "right" }}>
+              <Button component={NextLinkComposed} href="/posts/create">
+                Post
+              </Button>
+            </View>
+          )}
+          <EquallyGrid cols={4}>
+            {data.posts.data.map((post: any) => (
+              <Card key={post.id}>
+                <NextLink href="/posts/[id]" as={`/posts/${post.id}`} passHref>
+                  <Anchor co={{ textDecoration: "none", color: "black" }}>
+                    <Heading level={4}>{post.attributes.title}</Heading>
+                  </Anchor>
+                </NextLink>
+                <Text lineClamp={3}>{post.attributes.body}</Text>
+                <Divider />
+                <Text block align="right">
+                  {post.attributes.user.data.attributes.username}
+                </Text>
+              </Card>
+            ))}
+          </EquallyGrid>
+        </Stack>
       )}
     </Container>
   );
